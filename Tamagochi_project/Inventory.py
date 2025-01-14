45b734ec85536c211561bc44b8d772c65d3a597d
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-01-14 19:19:33 +0100

    Inventory

---
+++ Tamagochi_project/Inventory.py
@@ -0,0 +1,55 @@
+Class inventory()
+
+inventory.current = {
+
+}
+
+# maybe also add
+food = {
+    "Basic food": 2, # food with value (currency)
+    "Energy bar": 4,
+    "Superfood": 20,
+    #...
+}
+
+toy = {
+    "Simple ball": 10, # toys with value (currency)
+    "Frisbee": 30, 
+    "Jumping rope": 25,
+    #...
+}
+
+inventory.shop = {
+    food,
+    "medicine": 2,
+    toy
+}
+ 
+
+def inventory.purchase():
+    print(inventory.shop)
+
+def inventory.overview():
+    print(inventory.current)
+
+def inventory.open():
+    inventory.overview
+    while inventory.purchase == TRUE:
+        inventory.purchase
+    
+
+done
+
+def inventory.close():
+    print("\n ")
+    print(# main graphic surface)
+done
+
+
+
+
+
+
+
+    
+
